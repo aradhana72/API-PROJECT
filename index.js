a19bff16733aa78f75274e94e974ecc0a7405934
aradhana72 <aradhanakund@gmail.com>
2021-12-02 19:57:46 +0530

    Delete using mongoose

--- index.js
+++ index.js
@@ -171,6 +171,32 @@ booky.post("/publication/new", (req,res)=> {
   return res.json({updatedPublications: database.publication});
 });
 
+//Update a book title
+/*
+Route           /book/update/:isbn
+Description     update title of the book
+Access          Public
+Parameter       isbn
+Methods         PUT
+*/
+booky.put("/book/update/:isbn", async (req,res)=> {
+  const updatedBook = await BookModel.findOneAndUpdate(
+    {
+      ISBN: req.params.isbn
+    },
+    {
+      title: req.body.bookTitle
+    },
+    {
+      new: true
+    }
+  );
+
+  return res.json({books: database.books});
+});
+
+
+
 //UPADTE PUB AND BOOK
 /*
 Route           /publication/update/book
@@ -215,14 +241,12 @@ Parameter       isbn
 Methods         DELETE
 */
 
-booky.delete("/book/delete/:isbn", (req,res)=> {
-  const updateBookDatabase = database.books.filter(
-    (book) => book.ISBN !== req.params.isbn
-  )
+booky.delete("/book/delete/:isbn", async (req,res)=> {
+  const updateBookDatabase = await BookModel.findOneAndDelete({
+    ISBN: req.params.isbn
+  });
 
-  database.books = updateBookDatabase;
-
-  return res.json({books: database.books});
+  return res.json({books: updateBookDatabase});
 });
 
 //DELETE AN AUTHOR FROM A BOOK AND VICE VERSA
@@ -236,15 +260,19 @@ Methods         DELETE
 
 booky.delete("/book/delete/author/:isbn/:authorId", (req,res)=> {
   //Update the book db
-  database.books.forEach((book) => {
-    if(book.ISBN === req.params.isbn) {
-      const newAuthorList = book.author.filter(
-        (eachAuthor) => eachAuthor !== parseInt(req.params.authorId)
-      );
-      book.author = newAuthorList;
-      return;
-    }
-  });
+  const updatedBook = await BookModel.findOneAndUpdate(
+    {
+      ISBN: req.params.isbn
+    },
+    {
+     $pull: {
+       authors: parseInt(req.params.authorId)
+     }
+   },
+   {
+     new: true
+   }
+ );
   //Update author db
   database.author.forEach((eachAuthor) => {
     if(eachAuthor.id === parseInt(req.params.authorId)) {
---
+++ operator.js
@@ -0,0 +1,14 @@
+//MongoDb Operators
+
+//Update Operators -> Arithmatic / Logical operations
+//$inc -> increment (+1,+2,-1,-2)
+//$min
+//$max
+//$set -> sets a data
+//$unset -> removes a property from an object
+
+//Array
+//$push
+//$pull
+//$pop
+//$addToSet -> Duplicates not pushed again  -> unique 
